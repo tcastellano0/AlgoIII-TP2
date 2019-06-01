087e0dc1112d43357012332ad5247288ba14cfc9
fang.robinson <fang.robinson@gmail.com>
2019-06-01 18:25:18 -0300

    Cambio a privacidad de factor

--- src/DurabilidadConFactor.java
+++ src/DurabilidadConFactor.java
@@ -1,6 +1,6 @@
 public class DurabilidadConFactor extends Durabilidad {
 
-    protected double factor;
+    private double factor;
 
     public DurabilidadConFactor(double durabilidad, double factor){
         this.durabilidad = durabilidad;
