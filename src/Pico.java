5cbfaa7967ad3177e6e168546d9ce76a5c7836cb
tcastellano0 <tomas@itaptec.com.ar>
2019-06-01 17:50:16 -0300

    Durabilidad de picos

--- src/Pico.java
+++ src/Pico.java
@@ -1,20 +1,20 @@
 public class Pico extends Herramienta {
 
-	public Pico(int fuerza, double durabilidad) {
+	public Pico(int fuerza, Durabilidad unaDurabilidad) {
 		this.fuerza = fuerza;
-		this.durabilidad = durabilidad;
+		this.durabilidad = unaDurabilidad;
 	}
 	
 	public static Pico crearPicoDeMadera() {
-		return new Pico(2, 100);	
+		return new Pico(2, new DurabilidadConFactor(100, 1));	
 	}
 	
 	public static Pico crearPicoDePiedra() {
-		return new Pico(4, 200);
+		return new Pico(4, new DurabilidadConFactor(200, 1.5));
 	}
 	
 	public static Pico crearPicoDeMetal() {
-		return new Pico(12, 400);
+		return new Pico(12, new DurabilidadConFactor(400, 1));
 	}
-	
 }
+
