e3705573dc7cecff2b46630418be09401c451edb
federicofrontera <federicofrontera@gmail.com>
2019-06-01 09:25:31 -0300

    Hacha de piedra

--- test/HachaTest.java
+++ test/HachaTest.java
@@ -4,17 +4,32 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HachaTest {
     @Test
-    public void test01HachaDeMaderaSeInicializaConDurabilidad100(){
+    public void test01HachaDeMaderaSeInicializaConDurabilidad100() {
         HachaMadera hacha = new HachaMadera();
 
         assertEquals(hacha.getDurabilidad(), 100);
     }
+
     @Test
-    public void test02HachaDeMaderaSeInicializaConFuerza2(){
+    public void test02HachaDeMaderaSeInicializaConFuerza2() {
         HachaMadera hacha = new HachaMadera();
 
         assertEquals(hacha.getFuerza(), 2);
 
+    }
+
+    @Test
+    public void test03HachaDePiedraSeInicializaConDurabilidad200() {
+        HachaPiedra hacha = new HachaPiedra();
+
+        assertEquals(hacha.getDurabilidad(), 200);
+    }
+
+    @Test
+    public void test03HachaDePiedraSeInicializaConFuerza5() {
+        HachaPiedra hacha = new HachaPiedra();
+
+        assertEquals(hacha.getFuerza(), 5);
     }
 
 }
