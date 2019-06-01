9ed9dbe555552866d8c0e0ef7f9993903e39eca6
tcastellano0 <tomas@itaptec.com.ar>
2019-06-01 17:44:55 -0300

    Durabilidad en la herramienta

--- src/Durabilidad.java
+++ src/Durabilidad.java
@@ -3,5 +3,9 @@ public abstract class Durabilidad {
     protected double durabilidad;
 
     public abstract void desgastar(int fuerza);
+    
+    public double getDurabilidad() {
+    	return durabilidad;
+    } 
 
 }
--- src/Herramienta.java
+++ src/Herramienta.java
@@ -1,5 +1,6 @@
 public abstract class Herramienta implements Desgastable{
 	protected int fuerza;
+	protected Durabilidad durabilidad;
 
     public double getDurabilidad() {
         return this.durabilidad.getDurabilidad();
