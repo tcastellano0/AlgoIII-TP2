a5dbc3cfe6f65b347cbc3a32cd58f5104d9bcf95
federicofrontera <federicofrontera@gmail.com>
2019-06-09 11:08:00 -0300

    refactor de Tablero (perdon Nico)

--- src/Jugador/Tablero.java
+++ src/Jugador/Tablero.java
@@ -1,29 +1,29 @@
 package Jugador;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Tablero<T> {
 	
 	private int filas;
 	private int columnas;
-	private Celda<T>[][] celdas;
+	private List<Celda<T>> celdas = new ArrayList<>();
 	
 	public Tablero(int filas, int columnas) {
 		this.filas = filas;
 		this.columnas = columnas;
-		celdas = new Celda[this.filas][this.columnas]; //no se como hacer para sacar el warning ): pero funciona con el warning por ahora by NicolasWuFiuba
-		
-		for (int fila = 0; fila < this.filas; ++fila) {
-			for (int columna = 0; columna < this.columnas; ++columna) {
-				celdas[fila][columna] = new Celda<T>();
-			}
+
+		for (int i = 0; i < this.filas * this.columnas; i++) {
+			celdas.add(new Celda<T>());
 		}
 	}
 	
 	public void agregar(T item, int fila, int columna) {
-		celdas[fila-1][columna-1].setContenido(item);
+		celdas.get((fila - 1) * this.columnas + columna - 1).setContenido(item);
 	}
 	
 	public T sacar(int fila, int columna) {
-		return celdas[fila-1][columna-1].getContenido();
+		return celdas.get((fila - 1) * this.columnas + columna - 1).getContenido();
 	}
 
 }
