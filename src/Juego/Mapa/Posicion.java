1f35f69db0a721e4f2177070e1cd65e084b8fa6a
federicofrontera <federicofrontera@gmail.com>
2019-06-10 07:00:16 -0300

    agrego la clase Posicion, para verla hoy a la tarde

---
+++ src/Juego/Mapa/Posicion.java
@@ -0,0 +1,40 @@
+package Juego.Mapa;
+
+import java.util.Objects;
+
+public class Posicion {
+    private int coordenadaX;
+    private int coordenadaY;
+
+    public Posicion(int coordenadaX, int coordenadaY) {
+        this.coordenadaX = coordenadaX;
+        this.coordenadaY = coordenadaY;
+    }
+
+    public Posicion arriba(){
+        return new Posicion(this.coordenadaX,this.coordenadaY+1);
+    }
+    public Posicion abajo(){
+        return new Posicion(this.coordenadaX,this.coordenadaY-1);
+    }
+    public Posicion izquierda(){
+        return new Posicion(this.coordenadaX-1,this.coordenadaY);
+    }
+    public Posicion derecha(){
+        return new Posicion(this.coordenadaX+1, this.coordenadaY);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Posicion posicion = (Posicion) o;
+        return coordenadaX == posicion.coordenadaX &&
+                coordenadaY == posicion.coordenadaY;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(coordenadaX, coordenadaY);
+    }
+}
---
+++ test/PosicionTest.java
@@ -0,0 +1,28 @@
+import Juego.Mapa.Posicion;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class PosicionTest {
+    @Test
+    void test01DosPosicionesSonIgualesSiTienenLasMismasCoordenadas(){
+        Posicion pos1 = new Posicion(2, 2);
+        Posicion pos2 = new Posicion(2, 2);
+
+        Assertions.assertEquals(pos1, pos2);
+    }
+
+    @Test
+    void test02DosPosicionesSonDistintasSiTienenDistintaPrimeraCoordenada(){
+        Posicion pos1 = new Posicion(2, 2);
+        Posicion pos2 = new Posicion(3, 2);
+
+        Assertions.assertNotEquals(pos1, pos2);
+    }
+    @Test
+    void test03DosPosicionesSonDistintasSiTienenDistintaSegundaCoordenada(){
+        Posicion pos1 = new Posicion(2, 2);
+        Posicion pos2 = new Posicion(2, 3);
+
+        Assertions.assertNotEquals(pos1, pos2);
+    }
+}
