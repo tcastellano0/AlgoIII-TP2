7093784dba45f8c6fbf3aa1d9a2d05f3e540a0de
fang.robinson <fang.robinson@gmail.com>
2019-06-11 17:16:49 -0300

    revertido el for en esIgual para tableros

--- src/Juego/Mapa/TableroMateriales.java
+++ src/Juego/Mapa/TableroMateriales.java
@@ -7,31 +7,17 @@ public class TableroMateriales extends Tablero<Material> {
 
 	public TableroMateriales(int filas, int columnas) {
 		super(filas, columnas);
-		for(Posicion unaPosicion: celdas.keySet()){
-			this.poner(new MaterialNulo(), unaPosicion);
-		}
+		//for(Posicion unaPosicion: celdas.keySet()){
+		//	this.poner(new MaterialNulo(), unaPosicion);
+		//}
 	}
 
 	public boolean esIgual(TableroMateriales tablero) {
 
-		for(int x = 1; x <= 3; x++) {
-			for(int y = 1; y <= 3; y++) {
-				System.err.println("Posicion");
-				System.err.println(x);
-				System.err.println(y);
-				System.err.println("Veo Segundo Tablero");
-				tablero.ver(x,y);
-				System.err.println("Veo Primer Tablero");
-				this.ver(x,y);
-				if(!this.ver(x,y).esIgual(tablero.ver(x,y)))
-					return false;
-			}
-		}
-		/*
 		for(Posicion unaPosicion : celdas.keySet()) {
 			if (!this.ver(unaPosicion).esIgual(tablero.ver(unaPosicion)))
 				return false;
-		}*/
+		}
 
 		return true;
 	}
