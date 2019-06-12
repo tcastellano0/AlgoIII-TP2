3b101c5a76777dfe7a8ab86aabb57a87b892809f
federicofrontera <federicofrontera@gmail.com>
2019-06-12 13:24:02 -0300

    agregados test a Posicion

--- test/PosicionTest.java
+++ test/PosicionTest.java
@@ -27,4 +27,33 @@ public class PosicionTest {
 
         Assertions.assertNotEquals(pos1, pos2);
     }
+
+    @Test
+    void test04LaPosicionDeArribaEsLaQueTieneLaSegundaCoordenadaUnoMayor(){
+        Posicion pos1 = new Posicion(2, 2);
+        Posicion pos2 = new Posicion(2, 3);
+
+        Assertions.assertEquals(pos1.arriba(), pos2);
+    }
+    @Test
+    void test05LaPosicionDeAbajoEsLaQueTieneLaSegundaCoordenadaUnoMenor(){
+        Posicion pos1 = new Posicion(2, 2);
+        Posicion pos2 = new Posicion(2, 1);
+
+        Assertions.assertEquals(pos1.abajo(), pos2);
+    }
+    @Test
+    void test06LaPosicionDerechaEsLaQueTieneLaPrimeraCoordenadaUnoMayor(){
+        Posicion pos1 = new Posicion(2, 2);
+        Posicion pos2 = new Posicion(3, 2);
+
+        Assertions.assertEquals(pos1.derecha(), pos2);
+    }
+    @Test
+    void test07LaPosicionIzquierdaEsLaQueTieneLaPrimeraCoordenadaUnoMenor(){
+        Posicion pos1 = new Posicion(2, 2);
+        Posicion pos2 = new Posicion(1, 2);
+
+        Assertions.assertEquals(pos1.izquierda(), pos2);
+    }
 }
