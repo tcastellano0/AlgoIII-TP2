81f0723b1b432f590c5c6d7587b264ed1450e085
tcastellano0 <tomas@itaptec.com.ar>
2019-06-12 17:44:12 -0300

    Modificaciones en movimiento mas sus test

--- src/Juego/Jugador/Movimiento.java
+++ src/Juego/Jugador/Movimiento.java
@@ -2,14 +2,26 @@ package Juego.Jugador;
 
 import Juego.Mapa.Mapa;
 import Juego.Mapa.Posicion;
+import Juego.Mapa.UbicacionInvalidaException;
 
 public class Movimiento {
+	private Mapa mapa;
     private Direccion direccion = DireccionNorte.getInstancia();
+    
+    public Movimiento(Mapa mapa) {
+    	this.mapa = mapa;
+    }
 
-
-    public void avanzar(Jugador jugador){
-        Posicion nuevaPosicion = direccion.siguiente(jugador.getPosicion());
-        jugador.setPosicion(nuevaPosicion);
+    public void avanzar(Jugador jugador) {
+    	Posicion posicionJugador = jugador.getPosicion();
+        Posicion nuevaPosicion = direccion.siguiente(posicionJugador);
+        
+        try {
+        	mapa.poner(jugador, nuevaPosicion);
+        	jugador.setPosicion(nuevaPosicion);
+        	mapa.sacar(posicionJugador);
+    	}
+        catch(UbicacionInvalidaException e) { }  
     }
 
     public void rotar(){
--- test/MovimientoTest.java
+++ test/MovimientoTest.java
@@ -2,6 +2,7 @@ import Juego.Jugador.Direccion;
 import Juego.Jugador.DireccionNorte;
 import Juego.Jugador.Jugador;
 import Juego.Jugador.Movimiento;
+import Juego.Mapa.Mapa;
 import Juego.Mapa.Posicion;
 import org.junit.jupiter.api.Test;
 
@@ -9,23 +10,63 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class MovimientoTest {
+	
     @Test
-    void test01LaDireccionInicialDelMovimientoEsNorte(){
-        Movimiento movimiento = new Movimiento();
+    void testLaDireccionInicialDelMovimientoEsNorte(){
+    	Mapa mapa = new Mapa(20, 20);
+        Movimiento movimiento = new Movimiento(mapa);
         Direccion direccion = movimiento.getDireccion();
+        
         assertEquals(direccion, DireccionNorte.getInstancia());
     }
 
     @Test
-    void test02MoverUnJugadorModificaSuPosicion(){
-        Jugador jugador = new Jugador();
-        Movimiento movimiento = new Movimiento();
+    void testMoverUnJugadorModificaSuPosicion(){
+    	Mapa mapa = new Mapa(20, 20);
+    	Jugador jugador = new Jugador();
+        Movimiento movimiento = new Movimiento(mapa);
+        
         Posicion posicion = new Posicion(1,1);
+        
+        mapa.poner(jugador, posicion);
         jugador.setPosicion(posicion);
-
         movimiento.avanzar(jugador);
 
         assertNotEquals(jugador.getPosicion(), posicion);
+    }
+    
+    @Test
+    void testNoMoverUnJugadorNoDeberiaModificarSuPosicion() {
+    	Mapa mapa = new Mapa(20, 20);
+    	Jugador jugador = new Jugador();
+    	
+    	Posicion posicion = new Posicion(1,1);
+        
+        mapa.poner(jugador, posicion);
+        jugador.setPosicion(posicion);
+
+        assertEquals(jugador.getPosicion(), posicion);
+    }
+    
+    @Test
+    void testMoverUnJugadorNPasosDeberiaModificarLaPosicionDelJugadorNVeces() {
+    	Mapa mapa = new Mapa(20, 20);
+    	Jugador jugador = new Jugador();
+    	Movimiento movimiento = new Movimiento(mapa);
+    	
+    	int numeroPasos = 15;
+    	
+    	Posicion posicion = new Posicion(1,1);
+    	Posicion posicionProxima = new Posicion(1, 1 + numeroPasos);
+        
+        mapa.poner(jugador, posicion);
+        jugador.setPosicion(posicion);
+        
+        for(int i = 1; i <= numeroPasos; i++) {
+        	movimiento.avanzar(jugador);
+        }
+        
+        assertEquals(jugador.getPosicion(), posicionProxima);
     }
 
 /*    @Test
