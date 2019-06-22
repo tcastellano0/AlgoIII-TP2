e15cb49087997d5e53bff09b3319ab5ebee4e091
fang.robinson <fang.robinson@gmail.com>
2019-06-22 17:42:32 -0300

    Color de fondo para el juego

--- src/Vista/VistaMapa.java
+++ src/Vista/VistaMapa.java
@@ -6,8 +6,17 @@ import Juego.Mapa.Posicion;
 import Juego.Mapa.Tablero;
 import Juego.Mapa.Ubicable;
 import javafx.collections.ObservableList;
+
 import javafx.scene.Node;
 import javafx.scene.layout.GridPane;
+
+//Imports para el color de fondo
+import javafx.scene.paint.*;
+import javafx.geometry.Insets;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+
 
 public class VistaMapa extends GridPane {
     private static VistaMapa instancia = new VistaMapa();
@@ -15,6 +24,7 @@ public class VistaMapa extends GridPane {
     Jugador jugador = AlgoCraft.getInstancia().getJugador();
 
     private VistaMapa() {
+        this.setBackground(new Background(new BackgroundFill(Color.LIGHTGREEN, CornerRadii.EMPTY, Insets.EMPTY)));
         int filas = mapa.getFilas();
         int columnas = mapa.getColumnas();
         for (int i = 0; i < filas; i++) {
