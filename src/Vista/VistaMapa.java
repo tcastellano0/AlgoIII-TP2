01a3be92ce38eae7c17df96f7eb8133919e96bfa
federicofrontera <federicofrontera@gmail.com>
2019-06-22 11:53:58 -0300

    refactor

--- src/Vista/VistaMapa.java
+++ src/Vista/VistaMapa.java
@@ -5,6 +5,8 @@ import Juego.Jugador.Jugador;
 import Juego.Mapa.Posicion;
 import Juego.Mapa.Tablero;
 import Juego.Mapa.Ubicable;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -13,10 +15,23 @@ import javafx.scene.layout.GridPane;
 public class VistaMapa extends GridPane {
     Tablero<Ubicable> mapa = AlgoCraft.getInstancia().getMapa();
     Jugador jugador = AlgoCraft.getInstancia().getJugador();
+    Image imagenJugador = new Image("Vista/images/menu/jugador.jpg", 25, 25, false, true);
+    Image imagenMadera = new Image("Vista/images/menu/madera.jpg", 25, 25, false, true);
+    Image imagenPiedra = new Image("Vista/images/menu/piedra.jpg", 25, 25, false, true);
+    Image imagenMetal = new Image("Vista/images/menu/metal.jpg", 25, 25, false, true);
+    Image imagenDiamante = new Image("Vista/images/menu/diamante.jpg", 25, 25, false, true);
 
     public VistaMapa() {
         int filas = mapa.getFilas();
         int columnas = mapa.getColumnas();
+
+        EventHandler<ActionEvent> event = new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent e)
+            {
+                Button boton = (Button) e.getSource();
+                boton.setGraphic(new ImageView(imagenJugador));
+            }
+        };
 
         for (int i = 0; i < filas; i++) {
             for (int j = 0; j < columnas; j++) {
@@ -25,19 +40,19 @@ public class VistaMapa extends GridPane {
                     Image fondo = null;
                     switch (clase) {
                         case "class Juego.Jugador.Jugador":
-                            fondo = new Image("Vista/images/menu/jugador.jpg", 25, 25, false, true);
+                            fondo = imagenJugador;
                             break;
                         case "class Materiales.Madera":
-                            fondo = new Image("Vista/images/menu/madera.jpg", 25, 25, false, true);
+                            fondo = imagenMadera;
                             break;
                         case "class Materiales.Piedra":
-                            fondo = new Image("Vista/images/menu/piedra.jpg", 25, 25, false, true);
+                            fondo = imagenPiedra;
                             break;
                         case "class Materiales.Metal":
-                            fondo = new Image("Vista/images/menu/metal.jpg", 25, 25, false, true);
+                            fondo = imagenMetal;
                             break;
                         case "class Materiales.Diamante":
-                            fondo = new Image("Vista/images/menu/diamante.jpg", 25, 25, false, true);
+                            fondo = imagenDiamante;
                             break;
                     }
                     ImageView iw = new ImageView(fondo);
@@ -47,6 +62,7 @@ public class VistaMapa extends GridPane {
                     Image fondo = new Image("Vista/images/menu/pasto.jpg", 25, 25, false, true);
                     ImageView iw = new ImageView(fondo);
                     Button boton = new Button("", iw);
+                    boton.setOnAction(event);
                     this.add(boton, i, j);
                 }
 
